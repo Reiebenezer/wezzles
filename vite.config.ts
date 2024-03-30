4d72dc1f256992150f173dfe5b6c8f69dc664fcb
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2024-03-31 01:37:32 +0800

    fixed iframe reference issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     define: {
         "global": 'window'
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                index: './index.html',
+                preview: './src/preview/preview.html'
+            }
+        }
     }
 })
