65b8155c0a8077ccc0ba05c9d9092f1ca7223c38
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2024-04-05 20:35:30 +0800

    Add PWA functionality and logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+    plugins: [
+        VitePWA({
+            manifest: {
+                icons: [
+                    {
+                        src: '/logo.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        }),
+    ],
     define: {
         "global": 'window'
     },
